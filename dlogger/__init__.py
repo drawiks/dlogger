982b3c7f0b5dabd47f3183e7cea3d87307c2b6df
drawiks <147206917+drawiks@users.noreply.github.com>
2026-02-26 16:55:34 +0200

    fix: resolve unknown context issue in logging

--- dlogger/__init__.py
+++ dlogger/__init__.py
@@ -3,7 +3,7 @@ from .handlers import Handler, ConsoleHandler, FileHandler, LogRecord, Filter
 from .formatters import Formatter, SimpleFormatter
 from .filters import LevelFilter
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 __all__ = [
     "logger",
     "dLogger",
