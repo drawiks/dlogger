cb1925cacba660b9f706f8eeb6a0de77e12d6f58
drawiks <147206917+drawiks@users.noreply.github.com>
2026-02-27 10:10:07 +0200

    feat: add KeywordFilter and ModuleFilter

--- dlogger/filters/__init__.py
+++ dlogger/filters/__init__.py
@@ -1,4 +1,6 @@
 from ..handlers.base import Filter
 from .level import LevelFilter
+from .keyword import KeywordFilter
+from .module import ModuleFilter
 
-__all__ = ["Filter", "LevelFilter"]
+__all__ = ["Filter", "LevelFilter", "KeywordFilter", "ModuleFilter"]
